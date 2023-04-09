bf266ccc6ae2f41600a6e5c98e00af6e55457f2e
YerinKim929 <nirey929@gmail.com>
2023-04-09 17:37:14 +0900

    [Feat] MainVC와 PlannerVC를 버튼으로 연결

---
+++ CapstoneApp/PlannerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PlannerViewController.swift
+//  CapstoneApp
+//
+//  Created by Yerin Kim on 2023/04/09.
+//
+
+import Foundation
+import UIKit
+
+class PlannerViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+    }
+}
--- CapstoneApp/ViewController.swift
+++ CapstoneApp/ViewController.swift
@@ -12,8 +12,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
 
+        
+    }
+    
+    
+    @IBAction func successLogin(_ sender: Any) {
+        guard let nextVC = self.storyboard?.instantiateViewController(identifier: "PlannerViewController") as? PlannerViewController else { return }
+                
+                nextVC.modalTransitionStyle = .coverVertical
+                nextVC.modalPresentationStyle = .fullScreen
+                
+                self.present(nextVC, animated: true, completion: nil)
+    }
+    
 
 }
 
